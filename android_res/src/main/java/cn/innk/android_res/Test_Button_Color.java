d7810b66c9964023217beb89a16a835c831caa87
ronnyjiang <ronnyjiang@china-liantong.com>
2017-05-23 15:38:27 +0800

    add android_res module

---
+++ android_res/src/main/java/cn/innk/android_res/Test_Button_Color.java
@@ -0,0 +1,33 @@
+package cn.innk.android_res;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ronny on 17-5-23.
+ */
+
+public class Test_Button_Color extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
